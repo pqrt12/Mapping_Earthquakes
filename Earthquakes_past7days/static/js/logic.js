a6add5443871fc2c67f51a9e8cbc1d16039b4936
pqrt12 <54596601+pqrt12@users.noreply.github.com>
2020-04-10 14:17:37 -0700

    added light base layer, and tectonic plate boundary overlay layer

--- Earthquakes_past7days/static/js/logic.js
+++ Earthquakes_past7days/static/js/logic.js
@@ -12,26 +12,37 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     accessToken: API_KEY
 });
 
-// Create a base layer that holds both maps.
+// We create the dark view tile layer that will be an option for our map.
+let lights = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+//  Create a base layer that holds both maps.
 //  no difference seen w/, w/o quote marks.
 let baseMaps = {
     "Streets": streets,
-    Satellite: satelliteStreets
+    Satellite: satelliteStreets,
+    Lights: lights
 };
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
     center: [39.5, -98.5],
     zoom: 3,
-    layers: [streets]
+    layers: [streets],
+    'worldCopyJump': true
 });
 
 // Create the earthquake layer for our map.
 let earthquakes = new L.layerGroup();
+let tectonicPlates = new L.layerGroup();
 
 // We define an object that contains the overlays.
 // This overlay will be visible all the time.
 let overlays = {
+    "Tectonic Plates": tectonicPlates,
     Earthquakes: earthquakes
 };
 
@@ -41,8 +52,8 @@ L.control.layers(baseMaps, overlays).addTo(map);
 
 
 // Retrieve the earthquake GeoJSON data.
-usgsUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-d3.json(usgsUrl).then(function (data) {
+let usgsJson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+d3.json(usgsJson).then(function (data) {
     // This function returns the style data for each of the earthquakes we plot on
     // the map. We pass the magnitude of the earthquake into a function
     // to calculate the radius.
@@ -128,18 +139,43 @@ d3.json(usgsUrl).then(function (data) {
             "#ea2c2c"
         ];
 
+        //        div.innerHTML += "<style> i { font-size: large; font-weight: bold; /*font-style: arial;*/ } </style>";
         // Looping through our intervals to generate a label with a colored square for each interval.
         for (var i = 0; i < magnitudes.length; i++) {
             console.log(colors[i]);
             div.innerHTML +=
                 '<i style="background:' + colors[i] + '"></i> ' +
-                magnitudes[i] + 
+                magnitudes[i] +
                 (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-//                (magnitudes[i + 1] ? "-" + magnitudes[i + 1] + "<br>" : "+");                
-//  "-" and "&ndash;" are similar, but different.
+            //  "-" and "&ndash;" are similar, but different.
         }
         return div;
     };
 
     legend.addTo(map);
+});
+
+
+//  tectonic plate boundaries json.
+//  NOTE: "raw" must be selected in github.
+let tpbJson = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+d3.json(tpbJson).then(function (data) {
+    console.log(data);
+
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+        style: {
+            opacity: 1,
+            color: "darkorange",
+            weight: 2
+        },
+
+        //  a popup for plate boundary Name.
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("<b>Plate Boundary: " + feature.properties.Name + "<b>");
+        }
+    }).addTo(tectonicPlates);
+
+    //  Then we add the earthquake layer to our map.
+    tectonicPlates.addTo(map);
 });
